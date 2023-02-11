5c2e8cc5d90d306c3e17a4532a1adb5db059295b
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-02-11 17:07:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"
 const Join = (props) =>{
   return(
     <div id ="join">
+    {props.words.join(',')}
       {/* Access prop 'words' and print it using .join like words.join(',')*/}
     </div>
   )
@@ -12,7 +13,8 @@ const App = () => {
 
   return (
     <div id="main">
-      <Join  />
+   
+      <Join  words={arr}/>
     </div>
   )
 }
